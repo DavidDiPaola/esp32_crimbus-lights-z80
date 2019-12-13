3bca6e2449ff904198da31ff57276fc1d6a952b4
David DiPaola <365233-DavidDiPaola@users.noreply.gitlab.com>
2019-12-13 11:47:54 -0800

    Z80 code: now with 100% more blinkenlights.

--- z80-code/test.s
+++ z80-code/test.s
@@ -3,25 +3,25 @@
 .org 0x0000
 	ld c, 0x00
 loop:
-	call set_on
+	ld b, 0xFF
+	call set_all
+
+	ld b, 0x00
+	call set_all
+
 	jp loop
 
 
 
-set_on:
+set_all:
 	ld c, 0
-_a:
-	ld a, 0xFF
+1:
+	ld a, b
 	out (c), a
 	inc c
 	ld a, c
 	cp 0
-	jp nz, _a
+	jp nz, 1b
 
 	ret
-
-
-
-done:
-	jp done
 
